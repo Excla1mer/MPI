7872f879d466f362c9f0a5b96ab68c0c90993108
Sergey <s_samoilenko_99@mail.ru>
2021-09-14 21:55:28 +0700

    Code corrections

--- lib/lib.c
+++ lib/lib.c
@@ -1,6 +1,5 @@
 #include "lib.h"
 
-
 char *bin(uint32_t x)
 {
 	char *output = (char *)malloc(MAX_BIN_LEN * sizeof(char));
@@ -353,4 +352,126 @@ int decode_fi2(char *input_str, uint32_t **array, size_t *array_size)
 	*array = output;
 
 	return 0;
+}
+
+ABC_t *abc_init(char *alphabet, size_t *size)
+{
+	*size = strlen(alphabet);
+	ABC_t *abc = (ABC_t *)malloc(sizeof(ABC_t) * (*size));
+	if (abc == NULL)
+	{
+		printf("<%s:%d> malloc failed!", __func__, __LINE__);
+		return NULL;
+	}
+
+	for (int i = 0; i < (*size); ++i)
+	{
+		abc[i].key = i;
+		abc[i].value = alphabet[i];
+	}
+
+	return abc;
+}
+
+int get_key_from_val(ABC_t *abc, size_t size, char value)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		if (abc[i].value == value)
+		{
+			return abc[i].key;
+		}
+	}
+
+	printf("<%s:%d> Can't find value '%c' in alphabet", __func__, __LINE__, value);
+	return -1;
+}
+
+char get_val_from_key(ABC_t *abc, size_t size, int key)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		if (abc[i].key == key)
+		{
+			return abc[i].value;
+		}
+	}
+
+	printf("<%s:%d> Can't find key '%d' in alphabet", __func__, __LINE__, key);
+	return -1;
+}
+
+int abc_mtf_helper(ABC_t *abc, size_t size, char value)
+{
+	int tmp_key = get_key_from_val(abc, size, value);
+	for (int i = 0; i < size; ++i)
+	{
+		if (abc[i].value == value)
+		{
+			abc[i].key = 0;	
+		}
+		else if (abc[i].key < tmp_key)
+			abc[i].key++;
+	}
+
+	return 0;
+}
+
+void abc_print(ABC_t *abc, size_t size)
+{
+	printf("=============abc_print===========\n");
+	for (int i = 0; i < size; ++i)
+	{
+		printf("'%c': [%d]\n", abc[i].value, abc[i].key);
+	}
+	printf("=================================\n");
+
+	return;
+}
+
+int abc_mtf(ABC_t *abc, size_t abc_size, const char *input_data, uint32_t **array, size_t *array_size)
+{
+	char tmp_char;
+	*array_size = strlen(input_data);
+	uint32_t *output = (uint32_t*)malloc((*array_size) * sizeof(uint32_t));
+	if (output == NULL)
+	{
+		printf("<%s:%d> malloc failed!\n", __func__, __LINE__);
+		return -1;
+	}
+
+	for (int i = 0; i < *array_size; ++i)
+	{
+		tmp_char = input_data[i];
+
+		output[i] = get_key_from_val(abc, abc_size, tmp_char);
+		if (output[i] == -1)
+		{
+			printf("<%s:%d> get_key_from_val failed!", __func__, __LINE__);
+			return -1;
+		}
+		abc_mtf_helper(abc, abc_size, tmp_char);
+	}
+
+	*array = output;
+
+	return 0;
+}
+
+char *decode_mtf(ABC_t *abc, size_t abc_size, uint32_t *array, size_t array_size)
+{
+	char *output = (char *)malloc(sizeof(char) * array_size);
+	if (output == NULL)
+	{
+		printf("<%s:%d> malloc failed!\n", __func__, __LINE__);
+		return NULL;
+	}
+
+	for (int i = 0; i < array_size; ++i)
+	{
+		output[i] = get_val_from_key(abc, abc_size, array[i]);
+		abc_mtf_helper(abc, abc_size, output[i]);
+	}
+
+	return output;
 }
--- lib/lib.h
+++ lib/lib.h
@@ -8,6 +8,12 @@
 #define MAX_BIN_LEN 128
 #define BIT_32      0x80000000
 
+typedef struct ABC_s
+{
+	int  key;
+	char value;
+} ABC_t;
+
 char *bin(uint32_t x);
 char *bint(uint32_t x);
 
@@ -22,3 +28,14 @@ int bin_str_to_bin(char *bin_str, uint32_t *array_pos);
 int decode_fi0(char *input_str, uint32_t **array, size_t *array_size);
 int decode_fi1(char *input_str, uint32_t **array, size_t *array_size);
 int decode_fi2(char *input_str, uint32_t **array, size_t *array_size);
+
+ABC_t *abc_init(char *alphabet, size_t *size);
+
+int get_key_from_val(ABC_t *abc, size_t size, char value);
+char get_val_from_key(ABC_t *abc, size_t size, int key);
+
+int abc_mtf_helper(ABC_t *abc, size_t size, char value);
+void abc_print(ABC_t *abc, size_t size);
+int abc_mtf(ABC_t *abc, size_t abc_size, const char *input_data, uint32_t **array, size_t *array_size);
+
+char *decode_mtf(ABC_t *abc, size_t abc_size, uint32_t *array, size_t array_size);
