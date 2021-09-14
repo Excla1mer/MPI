b7402804e75d24f7957a889a595d51815364d763
Sergey <s_samoilenko_99@mail.ru>
2021-09-14 21:47:14 +0700

    add decode_mtf

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,6 +1,6 @@
 #include "../lib/lib.h"
 
-char *ABC = "abcde";
+char *ABC = "abcdefghijklmnopqrstuvwxyz_";
 
 typedef struct ABC_s
 {
@@ -41,6 +41,20 @@ int get_key_from_val(ABC_t *abc, size_t size, char value)
 	return -1;
 }
 
+char get_val_from_key(ABC_t *abc, size_t size, int key)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		if (abc[i].key == key)
+		{
+			return abc[i].value;
+		}
+	}
+
+	printf("<%s:%d> Can't find key '%d' in alphabet", __func__, __LINE__, key);
+	return -1;
+}
+
 int abc_mtf_helper(ABC_t *abc, size_t size, char value)
 {
 	int tmp_key = get_key_from_val(abc, size, value);
@@ -65,7 +79,6 @@ void abc_print(ABC_t *abc, size_t size)
 	{
 		printf("'%c': [%d]\n", abc[i].value, abc[i].key);
 	}
-
 	printf("=================================\n");
 
 	return;
@@ -100,6 +113,24 @@ int abc_mtf(ABC_t *abc, size_t abc_size, const char *input_data, uint32_t **arra
 	return 0;
 }
 
+char *decode_mtf(ABC_t *abc, size_t abc_size, uint32_t *array, size_t array_size)
+{
+	char *output = (char *)malloc(sizeof(char) * array_size);
+	if (output == NULL)
+	{
+		printf("<%s:%d> malloc failed!\n", __func__, __LINE__);
+		return NULL;
+	}
+
+	for (int i = 0; i < array_size; ++i)
+	{
+		output[i] = get_val_from_key(abc, abc_size, array[i]);
+		abc_mtf_helper(abc, abc_size, output[i]);
+	}
+
+	return output;
+}
+
 int main(int argc, char const *argv[])
 {
 	if (argc != 2)
@@ -132,7 +163,7 @@ int main(int argc, char const *argv[])
 	printf("\n");
 
 	char *tmp_ptr;
-	char code[1024] = "";
+	char code[2048] = "";
 	for (int i = 0; i < array_count; ++i)
 	{
 		tmp_ptr = fi2(array[i]);
@@ -151,6 +182,17 @@ int main(int argc, char const *argv[])
 		printf("%d ", array2[i]);
 	}
 	printf("\n");
+
+	ABC_t *abc_copy = abc_init(ABC, &abc_count);
+	if (abc == NULL)
+	{
+		printf("<%s:%d> abc_init failed!\n", __func__, __LINE__);
+		return -1;
+	}
+
+	char *decoded_mtf_str = decode_mtf(abc_copy, abc_count, array2, array_count2);
+
+	printf("decoded_mtf_str: '%s'\n", decoded_mtf_str);
 	free(array);
 	free(array2);
 	
